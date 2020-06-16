2f56821955327bf683869295b82ff8907415b5cf
Nunny Reyes <nunnyreyes@gmail.com>
2020-06-16 20:57:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 
 
 
-def display_board(board)
+def display_board(array)
   puts " #{board[0]}  | #{board[1]}   |  #{board[2]} "
   puts "-----------"
   puts " #{board[3]}  | #{board[4]}   | #{board[5]}  "
