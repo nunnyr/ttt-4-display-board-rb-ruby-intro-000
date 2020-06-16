4cfc38aa3318739a82b74bc5e752605e27c25d86
Nunny Reyes <nunnyreyes@gmail.com>
2020-06-16 20:20:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,9 +4,9 @@
 board = [" "," "," "," ","  "," "," "," "," "]
 
 def display_board(board)
-  rowOne =  puts "   |   |   "
-  rowTwo =  puts "-----------"
-  rowThree = puts "   |   |   "
-  rowFour = puts "-----------"
-  rowFive = puts "   |   |   "
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
